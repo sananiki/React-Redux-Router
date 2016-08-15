f47d34b1895b307c1687ce80c8e2aa1f88835acd
sananiki <157621575@qq.com>
2016-08-15 16:43:11 +0800

    '8-15

--- components/Addtodo.js
+++ components/Addtodo.js
@@ -1,23 +1,71 @@
 import React, { Component, PropTypes } from 'react';
-import { Input, Button ,message  } from 'antd';
+import { Form,Input,InputNumber, Button ,message,DatePicker  } from 'antd';
+const createForm = Form.create;
+const FormItem = Form.Item;
+
 export default class AddTodo extends Component {
   render() {
+
+    const formItemLayout = {
+      labelCol: { span: 7 },
+      wrapperCol: { span: 12 },
+    };
+
     return (
-      <div className="example-input">
-        <Input placeholder="需要添加的内容" ref="sa"   />
-        <Button type="primary" onClick={this.handleClick}>增加</Button>
-      </div>
+            <Form horizontal >
+        <FormItem
+          {...formItemLayout}
+          label="金额"        
+        >
+          <Input placeholder="金额"  ref="sa"/>
+        </FormItem>
+
+        <FormItem
+          {...formItemLayout}
+          label="日期"
+        >
+           <DatePicker defaultValue={new Date()} format="yyyy-MM-dd"  onChange={value => this.handleChange(value)}/>
+        </FormItem>
+
+        <FormItem
+          {...formItemLayout}
+          label="备注"
+        >
+          <Input placeholder="备注用途" ref="bz"
+          />
+        </FormItem>
+
+        
+
+        <FormItem wrapperCol={{ span: 12, offset: 7 }}>
+          <Button type="primary" onClick={this.handleClick}>确定</Button>
+          &nbsp;&nbsp;&nbsp;
+        </FormItem>
+      </Form>
     )
+  }  
+    handleChange(date) {
+    const date_time=date.toString();
+    console.log(date_time)
+
   }
+   
     handleClick = (e) => {
-    const node = this.refs.sa.refs.input
-    const text = node.value.trim()
+    const sa_node = this.refs.sa.refs.input
+    //console.log(sa_node)
+    const sa_value = sa_node.value.trim()
+    const bz_node = this.refs.bz.refs.input
+    const bz_value = bz_node.value.trim()
+   // const date_time_value = this.refs.date_time.refs.input.value.trim()
+
+    text = {
+      money:sa_value,
+      date:date_time,
+      bz:bz_value
+    }
     message.info('您增加的是: ' + text);
     this.props.onAdd(text)
-    node.value = ''
+    sa.value = ''
   }
 }
-
-AddTodo.propTypes = {
-  onAdd: PropTypes.func.isRequired
-}
+AddTodo = Form.create()(AddTodo);
--- components/Todolist.js
+++ components/Todolist.js
@@ -4,7 +4,7 @@ import { Card } from 'antd';
 export default class TodoList extends Component {
   render() {
     return (
-      <Card style={{ width: 300 }}>
+      <Card >
          {this.props.todos.map((todo, index) =>
           <Todo {...todo}
                 key={index}
